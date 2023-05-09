b01df68e01b981798e6f00c5660f688414d76cfc
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-09 10:06:00 -0600

    cards function

--- js/movies.js
+++ js/movies.js
@@ -1,9 +1,27 @@
-// fetch("https://lopsided-thrilling-leopon.glitch.me/movies").then(res => res.json()).then(data => {
-//         console.log(data);
-//     });
+(function () {
+    "use strict";
 
-fetch('https://lopsided-thrilling-leopon.glitch.me/movies').then(res => res.json()).then(data => data.forEach(data => {console.log(data)
-    $('#card-area').append("<div class='movie_card'><p class='title'>" + data.title.substring(0, 10))
-}));
+// ---------- create cards -----------
+    function createCards(title, director, rating, genre) {
+        $('#card-area').html(`
+             <div class="card-group m-2" style="width: 15rem;">
+               <img src="img/12-angry-men.jpeg" class="card-img-top" alt="...">
+             <div class="card-body">
+               <h5 class="card-title">${title}</h5>
+               <p class="card-text">${director}</p>
+               <p class="card-text">${rating}</p>
+               <p class="card-text">${genre}</p>
+               <a href="#" class="btn btn-primary">rating</a>
+             </div>
+             </div>`)
+    };
 
 
+// ----------- fetch data -------------
+    fetch('https://lopsided-thrilling-leopon.glitch.me/movies').then(res => res.json()).then(data => data.forEach(data => {
+        console.log(data)
+        $('#card-area').append("<div class='movie_card'><p class='title'>" + data.title.substring(0, 10))
+    }));
+
+
+})();
