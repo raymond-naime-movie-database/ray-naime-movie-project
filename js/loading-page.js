f8f84203bfe479ae9d871a8ea73e72272e149ad3
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-09 11:42:36 -0600

    add movie

--- js/loading-page.js
+++ js/loading-page.js
@@ -4,11 +4,11 @@
 // -------- hides loading div ---------------
     function hideLoader() {
         $('#loading').hide();
-        carouselPlaceholder();//adds placeholder carousel
-        cardsPlaceholder();//adds placeholder cards
+        // carouselPlaceholder();//adds placeholder carousel
+        // cardsPlaceholder();//adds placeholder cards
     }
 
-    setTimeout(hideLoader, 5000); //change to hide after fetch data comes back
+    setTimeout(hideLoader, 1000); //change to hide after fetch data comes back
 
 
 // -------- cards placeholder ---------------
@@ -83,43 +83,43 @@
 //
 //
 // // ------------ nav bar placeholder --------------
-//
-//     $('#n-a-v').html(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
-//   <div class="container-fluid">
-//     <a class="navbar-brand" href="#">Navbar</a>
-//     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//       <span class="navbar-toggler-icon"></span>
-//     </button>
-//     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-//       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-//         <li class="nav-item">
-//           <a class="nav-link active" aria-current="page" href="#">Home</a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#">Link</a>
-//         </li>
-//         <li class="nav-item dropdown">
-//           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//             Dropdown
-//           </a>
-//           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-//             <li><a class="dropdown-item" href="#">Action</a></li>
-//             <li><a class="dropdown-item" href="#">Another action</a></li>
-//             <li><hr class="dropdown-divider"></li>
-//             <li><a class="dropdown-item" href="#">Something else here</a></li>
-//           </ul>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link disabled">Disabled</a>
-//         </li>
-//       </ul>
-//       <form class="d-flex">
-//         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-//         <button class="btn btn-outline-success" type="submit">Search</button>
-//       </form>
-//     </div>
-//   </div>
-// </nav>`)
+
+    $('#n-a-v').html(`<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>`)
 
 
 // ------------- footer placeholder -------------
@@ -157,5 +157,37 @@
 //   </button>
 // </div>`)
 //     }
+
+
+// ------------ add movie -----------------
+    const urlMovies = 'https://lopsided-thrilling-leopon.glitch.me/movies';
+
+    function addMovie(movieData) {
+        const optionsPOST = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieData);
+        }
+        fetch(urlMovies, optionsPOST)
+            .then("Movie was created")
+            .catch("failed");
+    };
+
+
+    $('#create-movie-btn').click(function (e) {
+        e.preventDefault();
+        let movieAdded = {
+            "title": $('#input-title').val(),
+            "director": $('#input-director').val(),
+            "rating": $('#input-rating').val(),
+            "genre": $('#input-genre').val(),
+            "id": 9 // how to generate new id?
+        }
+        console.log(movieAdded);
+        addMovie(movieAdded);
+    });
+
 
 })();
