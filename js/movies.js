eb15de6e899342cb28093e0c12a942361a3bad2a
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-09 10:14:38 -0600

    cards

--- js/movies.js
+++ js/movies.js
@@ -3,7 +3,7 @@
 
 // ---------- create cards -----------
     function createCards(title, director, rating, genre) {
-        $('#card-area').html(`
+        $('#card-area').append(`
              <div class="card-group m-2" style="width: 15rem;">
                <img src="img/12-angry-men.jpeg" class="card-img-top" alt="...">
              <div class="card-body">
@@ -20,7 +20,12 @@
 // ----------- fetch data -------------
     fetch('https://lopsided-thrilling-leopon.glitch.me/movies').then(res => res.json()).then(data => data.forEach(data => {
         console.log(data)
-        $('#card-area').append("<div class='movie_card'><p class='title'>" + data.title.substring(0, 10))
+        var title = data.title;
+        var director = data.director;
+        var rating = data.rating;
+        var genre = data.genre;
+
+        createCards(title, director, rating, genre);
     }));
 
 
