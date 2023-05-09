e85ab4dd1161a86b8b441899a0dacdeb5045f164
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-09 14:53:54 -0600

    delete button

--- js/movies.js
+++ js/movies.js
@@ -4,17 +4,18 @@
     const urlMovies = 'https://lopsided-thrilling-leopon.glitch.me/movies';
 
 // ---------- create cards -----------
-    function createCards(title, director, rating, genre) {
+    function createCards(movie) {
         $('#card-area').append(`
              <div class="card-group m-2" style="width: 15rem;">
                <img src="img/12-angry-men.jpeg" class="card-img-top" alt="...">
              <div class="card-body">
-               <h5 class="card-title">${title}</h5>
-               <p class="card-text">Director: ${director}</p>
-               <p class="card-text">Genre: ${genre}</p>
-               <p class="card-text">Rating: ${rating}</p>
+               <h5 class="card-title">${movie.title}</h5>
+               <p class="card-text">Director: ${movie.director}</p>
+               <p class="card-text">Genre: ${movie.genre}</p>
+               <p class="card-text">Rating: ${movie.rating}</p>
+               <p class="card-text hidden">ID: ${movie.id}</p>
                
-               <button id="deleteButton" type="submit" class="btn btn-danger">Delete Movie</button>
+               <button id="delete-btn-${movie.id}" type="submit" class="deleteButton btn btn-danger">Delete Movie</button>
                
                
              </div>
@@ -23,60 +24,75 @@
 
 // ----------- fetch data -------------
     function getMovies() {
+        $('#card-area').html('');
+        fetch('https://lopsided-thrilling-leopon.glitch.me/movies').then(res => res.json()).then(data => data.forEach(movie => {
+            console.log(movie)
 
-        fetch('https://lopsided-thrilling-leopon.glitch.me/movies').then(res => res.json()).then(data => data.forEach(data => {
-            console.log(data)
-            var title = data.title;
-            var director = data.director;
-            var rating = data.rating;
-            var genre = data.genre;
-
-            createCards(title, director, rating, genre);
+            createCards(movie);
 
         }))
     }
 
     getMovies()
 
+    // ------------ add movie -----------------
+
+    function addMovie(movieData) {
+        const optionsPOST = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(movieData)
+        };
+        fetch(urlMovies, optionsPOST)
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+            .then(() => getMovies())
+    };
+
+
+    $('#create-movie-btn').click(function (e) {
+        e.preventDefault();
+        let movieAdded = {
+            "title": $('#input-title').val(),
+            "director": $('#input-director').val(),
+            "rating": $('#input-rating').val(),
+            "genre": $('#input-genre').val(),
+        }
+        console.log(movieAdded);
+        addMovie(movieAdded);
+    });
+
     // ----------- delete data -------------
+
     function deleteMovie() {
         const optionsDelete = {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-            }
+            },
+            // body: JSON.(${id})
         };
+        fetch(urlMovies, optionsDelete)
+            .then(response => console.log(response))
+            .catch(error => console.error(error))
+            .then(() => getMovies())
+    };
+
+    $('.deleteButton').click(function (e) {
+        e.preventDefault();
+
+        var btn = $(this).parent('h5').val();
 
 
-
-// ------------ add movie -----------------
-
-        function addMovie(movieData) {
-            const optionsPOST = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(movieData)
-            };
-            fetch(urlMovies, optionsPOST)
-                .then(response => console.log(response))
-                .catch(error => console.error(error));
-        };
+        console.log(btn)
+        // let movieDeleted = {
+        //     id: ${}
+        // }
+        // console.log(movieDeleted);
+        // deleteMovie(movieDeleted);
+    });
 
 
-        $('#create-movie-btn').click(function (e) {
-            e.preventDefault();
-            let movieAdded = {
-                "title": $('#input-title').val(),
-                "director": $('#input-director').val(),
-                "rating": $('#input-rating').val(),
-                "genre": $('#input-genre').val(),
-                "id": 9 // how to generate new id?
-            }
-            console.log(movieAdded);
-            addMovie(movieAdded);
-        });
-
-    }
 })();
